29e63c89e8695901f8d564295a12663f93c86bdc
lulla-by <thfl3363@hanmail.net>
2023-09-13 18:27:54 +0900

    [Style] PageNumber 버튼 컴포넌트 생성 #1

---
+++ components/buttons/PageNumberButton.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+interface ActiveProps {
+  active?: boolean;
+}
+interface DefautProps extends ActiveProps {
+  number: number;
+}
+export function PageNumberButton({ number, active }: DefautProps) {
+  return <NumberBox active={active}>{number}</NumberBox>;
+}
+
+const NumberBox = styled.button<ActiveProps>`
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+  border: 1px solid ${(props) => props.theme.colors.brown5};
+  background-color: ${(props) =>
+    props.active ? props.theme.colors.brown5 : props.theme.colors.white};
+  color: ${(props) =>
+    props.active ? props.theme.colors.brown1 : props.theme.colors.brown5};
+font-size: ${props=>props.theme.fontSize.discription};
+  &:hover {
+    background-color: ${(props) => props.theme.colors.brown5};
+    color: ${(props) => props.theme.colors.white};
+  }
+`;
