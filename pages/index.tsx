66bfc1baaf9c579cf5468774a5f619445764b2f7
lulla-by <thfl3363@hanmail.net>
2023-09-13 11:47:08 +0900

    [Style] Default Button 컴포넌트 생성 #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Button from "@/components/buttons/Button";
+
 export default function Home() {
   return (
     <>
       <h1>Movie Dog Main Page</h1>
+      <Button width="400" disabled/>
+      <Button width="500" active/>
+      <Button width="500" />
+      <Button  icon="Google" width="300"/>
+      <Button  icon="Github" width="300"/>
     </>
   )
 }
