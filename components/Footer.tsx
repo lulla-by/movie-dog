d6702c51a3b4eaf34934f9211946b607e102e87a
hardy-is-cat <whwjdguszz@naver.com>
2023-10-12 22:49:15 +0900

    [Refactor] 푸터 css 수정 및 모바일 대응 #20

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,32 +3,43 @@ import styled from 'styled-components';
 export default function Footer() {
   return (
     <Container>
-        <List>
-          <ListItem>MOVIE DOG</ListItem>
-          <ListItem>Github - hardy-is-cat, lulla-by</ListItem>
-          <ListItem>영화 DB 제공 - TMDB</ListItem>
-        </List>
+      <List>
+        <ListItem>MOVIE DOG</ListItem>
+        <ListItem>Github - hardy-is-cat, lulla-by</ListItem>
+        <ListItem>영화 DB 제공 - TMDB</ListItem>
+      </List>
     </Container>
   );
 }
 
 const Container = styled.footer`
   background-color: ${({ theme }) => theme.colors.brown8};
-  min-width: 1280px;
+  width: 100%;
+  padding: 50px 0;
 `;
 
 const List = styled.ul`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  width: 1280px;
-  height: 88px;
-  margin: auto;
+  gap: 20px;
+  max-width: 1200px;
+  padding: 0 20px;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
+
 const ListItem = styled.li`
   color: ${({ theme }) => theme.colors.brown1};
   font-size: ${({ theme }) => theme.fontSize.discription};
   font-weight: 400;
+
   &:first-child {
     font-size: ${({ theme }) => theme.fontSize.headline2};
     font-weight: 800;
