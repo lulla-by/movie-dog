78c30cd10f33ad4852a755ecbeb1d6723b579f52
lulla-by <thfl3363@hanmail.net>
2023-10-25 17:17:49 +0900

    [Add] 구글 소셜로그인시 userData firestore에 저장 #29

--- utils/UserLogin.ts
+++ utils/UserLogin.ts
@@ -3,7 +3,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
-import { authService } from '@/fbase';
+import { authService, db } from '@/fbase';
+import { doc, setDoc } from 'firebase/firestore';
 type UidType = {
   uid: string;
   state: boolean;
@@ -41,6 +42,8 @@ export const firebaseLogin = async (
   }
 };
 
+
+// 구글 로그인 함수
 export const GoogleLogin = async (): Promise<UidType> => {
   // Google 로그인을 위한 인증 공급자를 생성
   const provider = new GoogleAuthProvider();
@@ -50,34 +53,32 @@ export const GoogleLogin = async (): Promise<UidType> => {
     const result = await signInWithPopup(authService, provider);
     //액세스 토큰은 credential.accessToken에 저장
     const credential = await GoogleAuthProvider.credentialFromResult(result);
-    // 로그인 성공시 then 블럭
     const token = await credential?.accessToken;
     // 로그인한 사용자 정보는 result.user에 저장
     const user = await result.user;
 
     const uid = await user.uid;
 
-    const userData = {
+    let userData = {
       uid,
       state: true,
     };
 
-    return userData;
+    if (user.email) {
+      await setDoc(doc(db, 'users', user.email), {
+        email: user.email,
+        uid: user.uid,
+        nickName: user.displayName,
+      });
+    }
 
-    //  사용자의 이메일, 프로필 사진 URL, 사용자 이름 등과 같은 정보에 접근
-    // IdP data available using getAdditionalUserInfo(result)
+    return userData;
   } catch (error) {
     console.log(error);
     const userData = {
       uid: '',
       state: false,
     };
-
     return userData;
-    // 로그인중발생한 오류처리
-    // const errorCode = error.code;
-    // const errorMessage = error.message;
-    // const email = error.customData.email;
-    // const credential = GoogleAuthProvider.credentialFromError(error);
   }
 };
