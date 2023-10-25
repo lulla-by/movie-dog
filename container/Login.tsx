87722ffd7ae18e9dbe1d2ac9d3734625bd688703
lulla-by <thfl3363@hanmail.net>
2023-10-25 16:11:02 +0900

    [Feat] 구글 로그인 구현 #29

--- container/Login.tsx
+++ container/Login.tsx
@@ -3,7 +3,7 @@ import Input from '../components/input/Input';
 import ConfirmButton from '../components/buttons/ConfirmButton';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { firebaseLogin } from '@/utils/UserLogin';
+import { GoogleLogin, firebaseLogin } from '@/utils/UserLogin';
 import Link from 'next/link';
 import { useSetRecoilState } from 'recoil';
 import { LoginsState } from '@/stores/LoginState';
@@ -50,11 +50,22 @@ export default function Login() {
   // 로그인
   const login = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (id.trim().length <= 0 || password.trim().length <= 0) {
-      return;
-    }
-    const userData = await firebaseLogin(id, password);
+    const target = e.target as HTMLButtonElement;
+    const targetName = target.innerText;
+    let userData;
 
+    // 파이어베이스 로그인
+    if (targetName === '로그인') {
+      userData = await firebaseLogin(id, password);
+
+      // 구글 로그인
+    } else if (targetName === '구글 로그인') {
+      userData = await GoogleLogin();
+
+      // 에러 발생
+    } else {
+     throw new Error("올바르지않은 접근입니다")
+    }
     if (userData.state === false) {
       return;
     }
@@ -98,8 +109,14 @@ export default function Login() {
               width={48}
               icon="github"
               text="깃허브 로그인"
+              onClick={login}
             />
-            <ExtendsConfirmButton width={48} icon="google" text="구글 로그인" />
+            <ExtendsConfirmButton
+              width={48}
+              icon="google"
+              text="구글 로그인"
+              onClick={login}
+            />
           </ButtonContainer>
         </fieldset>
       </LoginBox>
--- utils/UserLogin.ts
+++ utils/UserLogin.ts
@@ -1,4 +1,8 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 import { authService } from '@/fbase';
 type UidType = {
   uid: string;
@@ -10,6 +14,13 @@ export const firebaseLogin = async (
   id: string,
   password: string,
 ): Promise<UidType> => {
+  if (id.trim().length <= 0 || password.trim().length <= 0) {
+    return {
+      uid: '',
+      state: false,
+    };
+  }
+
   try {
     const userCredential = await signInWithEmailAndPassword(
       authService,
@@ -22,10 +33,51 @@ export const firebaseLogin = async (
       state: true,
     };
   } catch (error) {
-    alert("이메일과 비밀번호를 확인해주세요")
+    alert('이메일과 비밀번호를 확인해주세요');
     return {
       uid: '',
       state: false,
     };
+  }
+};
+
+export const GoogleLogin = async (): Promise<UidType> => {
+  // Google 로그인을 위한 인증 공급자를 생성
+  const provider = new GoogleAuthProvider();
+
+  try {
+    // 팝업 창을 열어 Google 로그인을 수행
+    const result = await signInWithPopup(authService, provider);
+    //액세스 토큰은 credential.accessToken에 저장
+    const credential = await GoogleAuthProvider.credentialFromResult(result);
+    // 로그인 성공시 then 블럭
+    const token = await credential?.accessToken;
+    // 로그인한 사용자 정보는 result.user에 저장
+    const user = await result.user;
+
+    const uid = await user.uid;
+
+    const userData = {
+      uid,
+      state: true,
+    };
+
+    return userData;
+
+    //  사용자의 이메일, 프로필 사진 URL, 사용자 이름 등과 같은 정보에 접근
+    // IdP data available using getAdditionalUserInfo(result)
+  } catch (error) {
+    console.log(error);
+    const userData = {
+      uid: '',
+      state: false,
+    };
+
+    return userData;
+    // 로그인중발생한 오류처리
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // const email = error.customData.email;
+    // const credential = GoogleAuthProvider.credentialFromError(error);
   }
 };
