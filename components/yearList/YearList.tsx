9e93043acc2411d860038a8ae6641ff028f559e6
lulla-by <thfl3363@hanmail.net>
2023-10-28 21:47:47 +0900

    [Feat] YearItem 추가 및 YearList 렌더링 #38 - 작품별 카드인 YearItem 컴포넌트 추가 - map을 사용하여 렌더링

--- components/yearList/YearList.tsx
+++ components/yearList/YearList.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import YearItem from './YearItem';
 type YearMainsProps = {
   year: string;
   data: any;
   idx: string;
 };
 
+type Movie = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
 const YearList = ({ year, data, idx }: YearMainsProps) => {
-  console.log(data);
-  
+ 
   return (
     <WrapperBlock>
-     년도별 데이터
+      <CardListBlock>
+        {data &&
+          data.map((movie: Movie) => {
+            return <YearItem movie={movie} />;
+          })}
+      </CardListBlock>
     </WrapperBlock>
   );
 };
@@ -19,3 +40,27 @@ const YearList = ({ year, data, idx }: YearMainsProps) => {
 export default YearList;
 
 const WrapperBlock = styled.section``;
+
+const CardListBlock = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-bottom: 50px;
+
+  & > a {
+    display: block;
+    width: calc(50% - 10px);
+  }
+
+  @media (min-width: 768px) {
+    & > a {
+      width: calc(25% - 15px);
+    }
+  }
+
+  @media (min-width: 1200px) {
+    & > a {
+      width: calc(20% - 16px);
+    }
+  }
+`;
