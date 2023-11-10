21acf833096f9c722ec9cdca0db331f1dc4e732f
lulla-by <thfl3363@hanmail.net>
2023-11-10 22:19:51 +0900

    [Style] UserInfo 컴포넌트 마크업 및 CSS 완료 #50,#51

--- components/mypage/UserInfo.tsx
+++ components/mypage/UserInfo.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 function UserInfo() {
+  const noPosterUrl = new URL('../../public/nooposter.png', import.meta.url)
+    .href;
+
+  const genleList = ['공포', '액션', '로맨스', '코미디', '액션', '로맨스', '코미디', '액션', '로맨스', '코미디'];
   return (
     <UserInfoWrapper>
-      <div>OOO님의 마이페이지</div>
-      <div>선호하는 장르</div>
+      <UserInfoBox>
+        <LayoutBox>
+          <TitleBox>OOO님의 마이페이지</TitleBox>
+          <FigureBox>
+            <UserProfileImage src={noPosterUrl} alt="유저의 프로필 사진" />
+            <UserProfielCapion>OOO님의 프로필 사진</UserProfielCapion>
+          </FigureBox>
+          <ul>
+            <li>내가 본 영화 : 00개</li>
+            <li>찜 해 둔 영화 : 00개</li>
+            <li>내가 쓴 한 줄 평 : 00개</li>
+          </ul>
+        </LayoutBox>
+      </UserInfoBox>
+      <UserGenres>
+        <TitleBox>나의 선호 장르</TitleBox>
+        <GenresListBox>
+          {genleList.map((item) => (
+            <li>{item}</li>
+          ))}
+        </GenresListBox>
+      </UserGenres>
     </UserInfoWrapper>
   );
 }
@@ -13,5 +37,133 @@ function UserInfo() {
 export default UserInfo;
 
 const UserInfoWrapper = styled.section`
-  background-color: lightcoral;
+  margin: auto;
+  width: 83%;
+  @media (max-width: 768px) {
+    padding: 0px;
+    flex-direction: row;
+  }
+  @media (min-width: 769px) {
+    padding: 0px;
+    flex-direction: row;
+  }
+  @media (min-width: 1200px) {
+    margin: auto;
+    margin-top: 50px;
+    display: flex;
+    gap: 20px;
+    width: 83%;
+    padding-left: 20px;
+    padding-right: 20px;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+`;
+
+const LayoutBox = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: auto;
+  margin-top: 32px;
+  padding-left: 164px;
+
+  li {
+    margin-top: 8px;
+    font-size: ${({ theme }) => theme.fontSize.discription};
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+    color: ${({ theme }) => theme.colors.brown9};
+  }
+`;
+const UserInfoBox = styled.div`
+  position: relative;
+  min-width: 380px;
+  border: 1px solid ${({ theme }) => theme.colors.brown5};
+  background-color: ${({ theme }) => theme.colors.brown1};
+  height: 189px;
+  border-radius: 10px;
+  @media (max-width: 768px) {
+    margin: auto;
+    width: 100%;
+  }
+  @media (min-width: 769px) {
+    padding: 0px;
+    width: 80%;
+    margin: auto;
+  }
+  @media (min-width: 1200px) {
+    width: 50%;
+  }
+`;
+const UserGenres = styled.div`
+ min-width: 380px;
+  border: 1px solid ${({ theme }) => theme.colors.brown5};
+  background-color: ${({ theme }) => theme.colors.brown1};
+  height: 189px;
+  border-radius: 10px;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin: auto;
+  }
+  @media (min-width: 769px) {
+    padding: 0px;
+    width: 80%;
+    margin: auto;
+  }
+  @media (min-width: 1200px) {
+    width: 50%;
+  }
+
+  h2 {
+    margin-top: 32px;
+    margin-left: 32px;
+  }
+`;
+
+const GenresListBox = styled.ul`
+  margin-left: 32px;
+  margin-top: 16px;
+  width: calc(100% - 64px);
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px 12px;
+  overflow: auto;
+  max-height: 100px;
+  li {
+    font-size: ${({ theme }) => theme.fontSize.discription};
+    color: ${({ theme }) => theme.colors.brown5};
+    display: inline-block;
+    padding: 4px 12px;
+    line-height: 150%; 
+    border-radius: 4px;
+    border: 1px solid var(--brown-5, #c58555);
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const FigureBox = styled.figure`
+  position: absolute;
+  left: 32px;
+  top: 45px;
+`;
+const UserProfileImage = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+const UserProfielCapion = styled.figcaption`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  clip-path: polygon(0 0, 0 0, 0 0);
+`;
+const TitleBox = styled.h2`
+  font-size: ${({ theme }) => theme.fontSize.headline2};
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
