f1500e28b8fa98ed38ebde7ac2514277255a89d2
lulla-by <thfl3363@hanmail.net>
2023-11-10 19:11:51 +0900

    [Feat] 로그인한 유저만 마이페이지 접근 가능 #50,#51 - 로그인한 유저만 접근가능하도록 처리 - 로그인하지않은 유저는 알림창과 함께 로그인 페이지로 redirection

--- container/MyPage.tsx
+++ container/MyPage.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserInfo from '@/components/mypage/UserInfo';
 import UserMovieList from '@/components/mypage/UserMovieList';
 import UserReviewList from '@/components/mypage/UserReviewList';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 function MyPageContainer() {
+  const router = useRouter();
+  const [userData, setUserData] = useState({ uid: '', state: false });
+  const { uid, state } = userData;
+  
+  useEffect(() => {
+    const data = localStorage.getItem('userData');
+    if (!data) {
+      alert('로그인이 필요한 작업입니다');
+      router.push('/login');
+      return;
+    } else {
+      setUserData(JSON.parse(data));
+    }
+  }, []);
+
   return (
     <MyPageWrapper>
       <UserInfo />
