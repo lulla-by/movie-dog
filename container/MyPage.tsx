259868340daf672de2403848dface920c166941f
lulla-by <thfl3363@hanmail.net>
2023-11-14 21:15:03 +0900

    [Bugfix] vecel 에러 문제 해결 #50,#51 -  getStaticPaths와 getStaticProps삭제 - /mypage에서 useEffect 사용하여 데이터 get 및 props 전달

--- container/MyPage.tsx
+++ container/MyPage.tsx
@@ -1,26 +1,10 @@
-import React, {useEffect, useState } from 'react';
 import UserInfo from '@/components/mypage/UserInfo';
 import UserMovieList from '@/components/mypage/UserMovieList';
 import UserReviewList from '@/components/mypage/UserReviewList';
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
 import { UserDataProps } from '@/utils/type/UserDataType';
 
 function MyPageContainer({ likeArr, reviewArr }: UserDataProps) {
-  const router = useRouter();
-  const [userData, setUserData] = useState({ uid: '', state: false });
-
-  useEffect(() => {
-    const data = localStorage.getItem('userData');
-    if (!data) {
-      alert('로그인이 필요한 작업입니다');
-      router.push('/login');
-      return;
-    } else {
-      setUserData(JSON.parse(data));
-    }
-  }, []);
-
   return (
     <MyPageWrapper>
       <UserInfo likeArr={likeArr} reviewArr={reviewArr} />
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,3 +1,48 @@
+import MyPageContainer from '@/container/MyPage';
+import { useState, useEffect } from 'react';
+import { db } from '@/fbase';
+import { ReviewType } from '@/utils/type/UserDataType';
+import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+
 export default function MyPage() {
-  return <></>;
+  const router = useRouter();
+  const [likeArr, setLikeArr] = useState([]);
+  const [reviewArr, setReviewArr] = useState<ReviewType[]>([]);
+
+  const getData = async (uid: string) => {
+    const reviewQuery = query(collection(db, 'reviews'));
+    const reviewSnapshot = await getDocs(reviewQuery);
+    let allReviewArr: ReviewType[] = [];
+    reviewSnapshot.forEach((review) => {
+      allReviewArr.push(review.data() as ReviewType);
+    });
+    const reviewResult = allReviewArr.filter((item) => item.uid == uid);
+    setReviewArr(reviewResult);
+
+    const likesDocRef = doc(db, 'likes', uid);
+    const likesSnap = await getDoc(likesDocRef);
+    const likeResult = likesSnap.data()?.likedMovies;
+    setLikeArr(likeResult);
+  };
+
+  useEffect(() => {
+    const data = localStorage.getItem('userData') as string;
+    if (data == null) {
+      alert('로그인이 필요한 작업입니다');
+      router.push('/login');
+      return;
+    }
+
+    const { uid } = JSON.parse(data);
+    getData(uid);
+  }, []);
+
+  return (
+    <>
+      {likeArr.length !== 0 && reviewArr.length !== 0 && (
+        <MyPageContainer likeArr={likeArr} reviewArr={reviewArr} />
+      )}
+    </>
+  );
 }
