5e0b3ec07dcee99fca9bebec79ff79e8aafdb125
lulla-by <thfl3363@hanmail.net>
2023-11-14 23:53:50 +0900

    [Feat] 한줄평 렌더링 및 페이지 네비게이션 완성 #53

--- components/mypage/UserReviewList.tsx
+++ components/mypage/UserReviewList.tsx
@@ -1,17 +1,29 @@
 import { ReviewDataProps } from '@/utils/type/UserDataType';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ReviewBox from '../ReviewBox';
 import PageNavigatorButton from '../buttons/PageNavigatorButton';
 
 function UserReviewList({ reviewArr }: ReviewDataProps) {
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const renderFirstCardIdx = currentPage * 4 - 4;
+  const renderLastCardIdx = currentPage * 4;
+  const totalPage = Math.ceil(reviewArr.length / 4);
+  const [nextCount, setNextCount] = useState(10);
+  const lineRenderData = 4 * nextCount < reviewArr.length;
+
   const renderPagination = () => {
     const pageNumbers = [];
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= totalPage; i++) {
       pageNumbers.push(
         <PageNumButton
+        className={i === currentPage ? 'active' : ''}
           key={i}
+          onClick={() => {
+            moveIndex(i);
+          }}
+
         >
           {i}
         </PageNumButton>,
@@ -19,26 +31,68 @@ function UserReviewList({ reviewArr }: ReviewDataProps) {
     }
     return pageNumbers;
   };
-  const currentItems = renderPagination()
+
+  const moveIndex = (i: number) => {
+    setCurrentPage(i);
+  };
+
+  function calculateMinIndex(currentValue: number) {
+    const lowerBound = Math.floor((currentValue - 1) / 10) * 10 + 1;
+    return lowerBound;
+  }
+
+  function calculateMaxIndex(currentValue: number) {
+    const lowerBound = calculateMinIndex(currentValue);
+    const upperBound = lowerBound + 9;
+    return upperBound;
+  }
+  const [indexOfLastItem, setIndexOfLastItem] = useState(
+    calculateMaxIndex(currentPage),
+  );
+  const [indexOfFirstItem, setIndexOfFirstItem] = useState(
+    calculateMinIndex(currentPage),
+  );
+
+  const currentItems = renderPagination().slice(
+    indexOfFirstItem - 1,
+    indexOfLastItem,
+  );
+
+  const renderData = reviewArr.slice(renderFirstCardIdx, renderLastCardIdx);
+
   return (
     <ReviewListwrapper>
       <ReviewListBox>
         <h2>나의 한 줄 평</h2>
         <ul>
-          {reviewArr.map((review) => (
-            <li>
+          {renderData.map((review) => (
+            <li key={review.movieId}>
               <ReviewBox review={review} />
             </li>
           ))}
         </ul>{' '}
         <PageNavigation>
+          {indexOfFirstItem > 10 && (
             <PageNavigatorButton
               direction="prev"
+              onClick={() => {
+                setIndexOfFirstItem((prev) => prev - 10);
+                setIndexOfLastItem((prev) => prev - 10);
+                setNextCount((prev) => prev - 10);
+              }}
             />
+          )}
           {currentItems}
+          {lineRenderData && (
             <PageNavigatorButton
               direction="next"
+              onClick={() => {
+                setIndexOfFirstItem((prev) => prev + 10);
+                setIndexOfLastItem((prev) => prev + 10);
+                setNextCount((prev) => prev + 10);
+              }}
             />
+          )}
         </PageNavigation>
       </ReviewListBox>
     </ReviewListwrapper>
@@ -53,7 +107,7 @@ const ReviewListwrapper = styled.section`
 `;
 const ReviewListBox = styled.div`
   margin: 20px;
-  padding: 32px 34px 50px 32px;
+  padding: 32px 32px 50px 32px;
   background-color: antiquewhite;
   border: 1px solid ${({ theme }) => theme.colors.brown5};
   background-color: ${({ theme }) => theme.colors.brown1};
