d2e62ac64b4eb1ed4f4aa0808cb4e5c2979c63fb
lulla-by <thfl3363@hanmail.net>
2024-02-28 23:42:56 +0900

    로직 변경

--- pages/list/year/[...parmas].tsx
+++ pages/list/year/[...parmas].tsx
@@ -2,6 +2,7 @@ import Category from '@/components/yearList/Category';
 import YearList from '@/components/yearList/YearList';
 import { Movie } from '@/utils/type/MovieType';
 import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
 import React from 'react';
 import styled from 'styled-components';
@@ -13,10 +14,11 @@ interface YearMainProps {
 }
 
 function YearMain({ data, year, idx }: YearMainProps) {
+ 
   return (
     <WrapperBlock>
       <Category />
-      {/* <YearList year={year} data={data} idx={idx} /> */}
+      <YearList year={year} data={data} idx={idx} />
     </WrapperBlock>
   );
 }
@@ -49,34 +51,32 @@ const WrapperBlock = styled.div`
   }
 `;
 
-// export const getServerSideProps = async (
-//   context: GetServerSidePropsContext,
-// ) => {
-//   const params: ParsedUrlQuery | undefined = context.params;
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const params = context.params;
 
-//   //params가 undefined인지 확인하고, 해당 경우 기본값인 year와 idx를 설정
-//   let year = '2020';
-//   let idx = '1';
+  // params가 undefined인지 확인하고, 해당 경우 기본값인 year와 idx를 설정
+  let year = '2020';
+  let idx = '1';
 
-//   if (params && params.parmas) {
-//     // params.parmas가 존재하고 배열인지 여부를 확인하고, 값에 접근하여 year와 idx 변수를 설정
-//     const paramArray = Array.isArray(params.parmas)
-//       ? params.parmas
-//       : [params.parmas];
-//     year = paramArray[0] as string;
-//     idx = paramArray[1] ? (paramArray[1] as string) : '1';
-//   }
+  if (params && params.year) {
+    // params.year가 존재하고 배열인지 여부를 확인하고, 값에 접근하여 year와 idx 변수를 설정
+    const paramArray = Array.isArray(params.year)
+      ? params.year
+      : [params.year];
+    year = paramArray[0] as string;
+    idx = params.idx ? (params.idx as string) : '1';
+  }
 
-//   const response = await fetch(
-//     `https://movie-dog.vercel.app/api/movie/${year}/${idx}`,
-//   );
-//   const { results } = await response.json();
+  const response = await fetch(
+    `https://movie-dog.vercel.app/api/movie/${year}/${idx}`,
+  );
+  const { results } = await response.json();
 
-//   return {
-//     props: {
-//       data: results,
-//       year: year,
-//       idx: idx,
-//     },
-//   };
-// };
+  return {
+    props: {
+      data: results,
+      year,
+      idx,
+    },
+  };
+};
